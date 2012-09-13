2dff5e04b0836c02ec77edfa781aecee94565106
Vincent Paca <vincent.paca@gmail.com>
2012-09-14 00:06:09 +0800

    Added logo to companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ActiveRecord::Base
-  attr_accessible :address, :description, :name
+  attr_accessible :address, :description, :name, :logo
   has_many :users
+
+  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120913220822) do
+ActiveRecord::Schema.define(:version => 20120913220823) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -50,8 +50,12 @@ ActiveRecord::Schema.define(:version => 20120913220822) do
     t.string   "name"
     t.string   "description"
     t.string   "address"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
   end
 
   create_table "users", :force => true do |t|
