ea986b1edd3e6f9c9bf680afb88a21b195db3333
Vincent Paca <vincent.paca@gmail.com>
2012-09-13 20:19:53 +0800

    Initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Staffwisely::Application.config.secret_token = 'adac4455cb7bd6ecf72c2233da1fa14586a56878f6f6983d9d0aad83080e62eabdd96744a3c84151dfa0d771ddb95b8dc33ddbc1fdd2aa67ff3485e9f322f08a'
