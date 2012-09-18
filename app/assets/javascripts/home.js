479beb7495395cabfbb43417e50fb8a9e908591e
Vincent Paca <vincent.paca@gmail.com>
2012-09-18 23:36:14 +0800

    Added notification for successful signup

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,5 +2,6 @@ $(document).on('click', '#notify-me', function(){
   var email = $('#notify-email').val();
   $.post("/notify_me.json", { email: email }, function(){
     $('#notify-email').val("");
+    alert("Thank you for signing up!");
   });
 });
