e4b1651d74261281170b1d551ee307539ad1bb86
Vincent Paca <vincent.paca@gmail.com>
2012-09-18 23:08:19 +0800

    Added code to disallow access to app and registration

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,5 +1,11 @@
 class AppController < ApplicationController
-  
+  before_filter :disallow_access
+
   def show
+  end
+
+  protected
+  def disallow_access
+    redirect_to root_url
   end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_filter :disallow_access
+
   protected
 
   def after_inactive_sign_up_path_for(resource)
     new_app_company_path
+  end
+
+  def disallow_access
+    redirect_to root_url
   end
 end
