992a79f13cddc55939ae4995328b1cc6239de979
Vincent Paca <vincent.paca@gmail.com>
2012-09-18 21:28:38 +0800

    Update session store to be compatible with ruby 1.8

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Staffwisely::Application.config.session_store :cookie_store, key: '_staffwisely_session'
+Staffwisely::Application.config.session_store :cookie_store, key => '_staffwisely_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
