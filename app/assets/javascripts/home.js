33bf8d672dbed348343ed43f1f0d938189505e92
Vincent Paca <vincent.paca@gmail.com>
2012-09-19 22:54:24 +0800

    Made the notifications pretty

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,22 @@
 $(document).on('click', '#notify-me', function(){
   var email = $('#notify-email').val();
-  $.post("/notify_me.json", { email: email }, function(){
-    $('#notify-email').val("");
-    alert("Thank you for signing up!");
+
+  if (email == ""){
+    $(".notification-tip.error").show();
+    return false;
+  }
+
+  $.post("/notify_me.json", { email: email }, function(data){
+    $('.input-wrapper').hide();
+    $('.loader').show();
+    if(data.success){
+      $('.loader').hide();
+      $('.input-wrapper').show();
+      $('.input-wrapper').html("<h3>Thanks! We'll keep you posted.</h3>");
+    }else{
+      $('.loader').hide();
+      $('.input-wrapper').show();
+      $('.notification-tip.error').show();
+    }
   });
 });
