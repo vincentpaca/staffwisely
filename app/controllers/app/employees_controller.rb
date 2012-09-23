1eb0d82d810a33798153ad18636df08d8944346c
Vincent Paca <vincent.paca@gmail.com>
2012-09-24 00:52:42 +0800

    Added view for creating new employee

--- app/controllers/app/employees_controller.rb
+++ app/controllers/app/employees_controller.rb
@@ -1,2 +1,11 @@
-class EmployeesController < AppController
+class App::EmployeesController < AppController
+
+  def index
+    redirect_to app_path
+  end
+  
+  def new
+    @employee = Employee.new
+  end
+
 end
