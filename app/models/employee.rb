94250c2b119394afac77d8bf4582e45a37475cfe
Vincent Paca <vincent.paca@gmail.com>
2012-09-24 00:15:16 +0800

    Added rate to employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,8 @@
 class Employee < ActiveRecord::Base
-  attr_accessible :added_by, :category, :company_id, :experience, :location, :position, :skill_level, :full_name, :picture
+  attr_accessible :added_by, :category, :company_id, :experience, :location, :position, :skill_level, :full_name, :picture, :rate
   belongs_to :company
 
   has_attached_file :picture, :styles => { :thumb => "80x80>" }
+
+  validates_presence_of :full_name, :position, :location
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120923144741) do
+ActiveRecord::Schema.define(:version => 20120923153646) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(:version => 20120923144741) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.integer  "rate"
   end
 
   create_table "users", :force => true do |t|
