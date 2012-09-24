59c62f9997c3ab6edc9c65eb3c680a1a9cc92a4c
Vincent Paca <vincent.paca@gmail.com>
2012-09-24 22:07:34 +0800

    Added categories in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+category = Category.create(:name => "Programming & IT")
+SubCategory.create(:category => category, :name => "Web Development")
+SubCategory.create(:category => category, :name => "Software Development")
+SubCategory.create(:category => category, :name => "Mobile Development")
+SubCategory.create(:category => category, :name => "QA")
+SubCategory.create(:category => category, :name => "Project Management")
+SubCategory.create(:category => category, :name => "Network and System")
+SubCategory.create(:category => category, :name => "Administration")
+SubCategory.create(:category => category, :name => "Other")
+
+category = Category.create(:name => "Design & Multimedia")
+SubCategory.create(:category => category, :name => "UI Design")
+SubCategory.create(:category => category, :name => "Graphics & Illustration")
+SubCategory.create(:category => category, :name => "Animation")
+SubCategory.create(:category => category, :name => "Other")
+SubCategory.create(:category => category, :name => "Other")
+
+category = Category.create(:name => "Writing & Translation")
+SubCategory.create(:category => category, :name => "Articles")
+SubCategory.create(:category => category, :name => "Blogs")
+SubCategory.create(:category => category, :name => "Copywriting")
+SubCategory.create(:category => category, :name => "Technincal Writing")
+SubCategory.create(:category => category, :name => "Translations")
+SubCategory.create(:category => category, :name => "Other")
+
+category = Category.create(:name => "Admin & Support")
+SubCategory.create(:category => category, :name => "Virtual Assistant")
+SubCategory.create(:category => category, :name => "Data Entry")
+SubCategory.create(:category => category, :name => "Accounting")
+SubCategory.create(:category => category, :name => "Legal")
+SubCategory.create(:category => category, :name => "Customer Service")
+SubCategory.create(:category => category, :name => "Technical Support")
+SubCategory.create(:category => category, :name => "Other")
+
+
