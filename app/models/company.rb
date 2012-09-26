e624979e64c8bb090bacbbf028958c9cbc3a2f95
Vincent Paca <vincent.paca@gmail.com>
2012-09-26 14:22:01 +0800

    Require name in company creation

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,6 @@ class Company < ActiveRecord::Base
   has_many :employees
 
   has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "230x78>" }
+
+  validates_presence_of :name
 end
