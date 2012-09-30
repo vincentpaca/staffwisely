369cb26f38c28815765488df798637b84b9cbf78
Vincent Paca <vincent.paca@gmail.com>
2012-09-30 17:55:15 +0800

    Updated mina config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,23 +2,14 @@ require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
 
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
-
 set :domain, 'staffwisely.com'
 set :deploy_to, '/staffwisely'
 set :repository, 'https://github.com/vincentpaca/staffwisely.git'
 set :branch, 'master'
 
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', 'log']
 
 set :user, 'staffwly'
-set :passwodd, 's8dorimi38'
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
