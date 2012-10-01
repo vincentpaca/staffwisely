263da2fe55f290d4474f2de7738b90dabbcbf680
Vincent Paca <vincent.paca@gmail.com>
2012-10-02 00:12:16 +0800

    Added default value for notifications

---
+++ db/migrate/20121001154425_add_default_value_to_read.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToRead < ActiveRecord::Migration
+  def change
+    change_column :notifications, :read, :boolean, :default => false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121001150625) do
+ActiveRecord::Schema.define(:version => 20121001154425) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -126,10 +126,10 @@ ActiveRecord::Schema.define(:version => 20121001150625) do
   create_table "notifications", :force => true do |t|
     t.integer  "company_id"
     t.string   "message"
-    t.boolean  "read"
+    t.boolean  "read",       :default => false
     t.integer  "sender_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
   end
 
   create_table "sub_categories", :force => true do |t|
