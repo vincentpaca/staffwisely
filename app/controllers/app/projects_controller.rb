cb45707bda8ca9661b5e63cddd7e0e853810888a
Vincent Paca <vincent.paca@gmail.com>
2012-10-04 01:31:49 +0800

    Added project views

--- app/controllers/app/projects_controller.rb
+++ app/controllers/app/projects_controller.rb
@@ -1,8 +1,11 @@
 class App::ProjectsController < AppController
   def index
+    @pending_projects = current_company.employee_projects
+    @pending_projects << current_company.employer_projects
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def create
