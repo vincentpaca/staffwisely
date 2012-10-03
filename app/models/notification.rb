f2e13c3c16a4e4b4b123c6ec310a142b2192d5ce
Vincent Paca <vincent.paca@gmail.com>
2012-10-03 20:27:37 +0800

    Added mailer

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,14 @@ class Notification < ActiveRecord::Base
 
   belongs_to :sender, :class_name => 'User'
   belongs_to :company
+
+  after_create :notify_all_users
+
+  private
+
+  def send_email(email)
+    emails = self.company.users.collect(&:email).join(",")
+    NotificationMailer.delay.notify(emails)
+  end
+  handle_asynchronously :send_email, :queue => "notifications"
 end
