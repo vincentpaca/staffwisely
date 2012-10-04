27e6787aa6bd830139d1df0671de7771f71908f4
Vincent Paca <vincent.paca@gmail.com>
2012-10-05 01:41:00 +0800

    Removed unused js

--- app/assets/javascripts/comments.js
+++
@@ -1,5 +0,0 @@
-$(document).on('click', '#submit-comment', function(e){
-  e.preventDefault();
-  var message = $('#comment').val();
-  $.post('/app/comments/create', { project_id: 1, message: message });
-})
