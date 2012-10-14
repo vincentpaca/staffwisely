4fd65b9f7736dd6fba3a6fbe13e068e0a809b52c
Vincent Paca <vincent.paca@gmail.com>
2012-10-14 17:45:14 +0800

    Show projects only to involved companies

--- app/controllers/app/projects_controller.rb
+++ app/controllers/app/projects_controller.rb
@@ -1,4 +1,6 @@
 class App::ProjectsController < AppController
+  before_filter :companies_involved, :only => [:show]
+
   def index
     @pending_projects = current_company.pending_projects
     @on_going_projects = current_company.on_going_projects
@@ -11,5 +13,15 @@ class App::ProjectsController < AppController
 
   def create
     @project = Project.new(params[:project])
+  end
+
+  protected
+
+  def companies_involved
+    @project = Project.find(params[:id])
+    unless current_company.eql?(@project.employer) || current_company.eql?(@project.employee)
+      flash[:alert] = "You do not have enough permissions to view this project"
+      redirect_to "/app"
+    end
   end
 end
